cdba546bd46fac44d1f4dccee29c13b02a1356a4
Koichi Murakami <koichi.murakami@mbc-net.com>
2024-08-29 14:16:27 +0900

    modify docusaurus.config.ts to fixed editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/mbc-net/mbc-cqrs-serverless-doc",
+          editUrl: "https://github.com/mbc-net/mbc-cqrs-serverless-doc/edit/master",
         },
         blog: false,
         theme: {
