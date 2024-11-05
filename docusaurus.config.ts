86e39c083cec1ee1e0e4fc3c96f49fe6696efea4
Koichi Murakami <koichi.murakami@mbc-net.com>
2024-11-05 16:06:19 +0900

    Update website URL and baseUrl for new deployment
    Changed the website's production URL to "https://mbc-cqrs-serverless.mbc-net.com" and simplified the baseUrl to "/". This adjustment is to accommodate the new deployment setup and ensure correct site navigation.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://mbc-net.github.io",
+  url: "https://mbc-cqrs-serverless.mbc-net.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/mbc-cqrs-serverless-doc/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
